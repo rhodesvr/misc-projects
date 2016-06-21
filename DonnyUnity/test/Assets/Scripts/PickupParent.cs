3060237b2069a91ce3996ed6f2e1f9a5152b5d6b
rhodesvr <rhodesvrlab@gmail.com>
2016-06-21 11:16:46 -0500

    first commit

---
+++ DonnyUnity/test/Assets/Scripts/PickupParent.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickupParent : MonoBehaviour
+{
+
+    SteamVR_TrackedObject trackedobj;
+    
+
+    void Awake()
+    {
+        trackedobj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+
+    void FixedUpdate()
+    {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedobj.index);
+
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You are holding down the trigger");
+        }
+
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        SteamVR_Controller.Device device = SteamVR_Controller.Input((int)trackedobj.index);
+        Debug.Log("You have collided with " + col.name + " and activated OnTriggerStay");
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have collided with " + col.name + " while holding down touch");
+            col.attachedRigidbody.isKinematic = true;
+            col.gameObject.transform.SetParent(gameObject.transform);
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            Debug.Log("You have released Touch while colliding with " + col.name);
+            col.gameObject.transform.SetParent(null);
+            col.attachedRigidbody.isKinematic = false;
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# this is a .git for all misc projects.
+
+#IMPORTANT: when adding new projects to folder, be sure to update the .gitignore file with the directories of the Unity projects Temp and Library folders.
